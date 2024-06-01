9d931e260a777cf6ac587cef68bfab448c53ec6c
Mateusz Gazda <matigazda29@gmail.com>
2024-06-01 17:15:07 +0200

    Added uploading file to s3 and storing information in db

--- s3_file_manager_backend/Program.cs
+++ s3_file_manager_backend/Program.cs
@@ -3,6 +3,11 @@ using Microsoft.Extensions.Configuration;
 using s3_file_manager_backend.Data;
 using s3_file_manager_backend.Models;
 using System.Globalization;
+using Amazon;
+using Amazon.S3;
+using Microsoft.AspNetCore.Mvc;
+using s3_file_manager_backend.Services;
+using Microsoft.AspNetCore.Antiforgery;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +17,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<FileDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("FileDb")));
-
+builder.Services.AddSingleton<S3Service>();
+builder.Services.AddAntiforgery(options =>
+{
+    options.Cookie.Name = ".AspNetCore.Antiforgery.4-a-mNcJ_3w";
+});
 
 var app = builder.Build();
 
@@ -25,16 +34,39 @@ if (app.Environment.IsDevelopment())
 
 CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
 
-app.MapPost("/uploadfile", async (FileDbContext dbContext, StoredFile file) =>
+app.UseAntiforgery();
+
+app.MapGet("antiforgery/token", (IAntiforgery forgeryService, HttpContext context) =>
 {
-    // Add the provided file to the database
-    await dbContext.Files.AddAsync(file);
+    var tokens = forgeryService.GetAndStoreTokens(context);
+    var xsrfToken = tokens.RequestToken!;
+    return TypedResults.Content(xsrfToken, "text/plain");
+});
+
+app.MapPost("/uploadfile", async (IFormFile file, string fileName, FileDbContext dbContext, S3Service s3Service) =>
+{
+    if (file == null || string.IsNullOrEmpty(fileName))
+    {
+        return Results.BadRequest("Missing file or file name.");
+    }
+
+    var fileGuid = Guid.NewGuid();
+    var keyName = fileGuid.ToString();
+    await s3Service.UploadFileAsync(file, keyName);
+    var fileLink = s3Service.GenerateFileLink(keyName);
+
+    var storedFile = new StoredFile()
+    {
+        Id = fileGuid,
+        Name = fileName,
+        FileLink = fileLink
+    };
+
+    await dbContext.Files.AddAsync(storedFile);
     await dbContext.SaveChangesAsync();
 
-    // Return a response indicating success
-    return Results.Created($"/file/{file.Id}", file);
+    return Results.Created($"/file/{storedFile.Id}", storedFile);
 })
-.WithName("PostFile")
 .WithOpenApi();
 
 app.Run();
---
+++ s3_file_manager_backend/Services/S3Service.cs
@@ -0,0 +1,61 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.S3.Model;
+
+namespace s3_file_manager_backend.Services
+{
+    public class S3Service
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly string _bucketName;
+
+        public S3Service(IConfiguration configuration)
+        {
+            var awsAccessKeyId = configuration["AWS:AccessKey"];
+            var awsSecretAccessKey = configuration["AWS:SecretKey"];
+            var awsSessionToken = configuration["AWS:SessionToken"];
+            var region = configuration["AWS:Region"];
+            _bucketName = configuration["AWS:BucketName"];
+
+            _s3Client = new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, awsSessionToken, RegionEndpoint.GetBySystemName(region));
+        }
+
+        public async Task UploadFileAsync(IFormFile file, string keyName)
+        {
+            using (var newMemoryStream = new MemoryStream())
+            {
+                file.CopyTo(newMemoryStream);
+                var uploadRequest = new TransferUtilityUploadRequest
+                {
+                    InputStream = newMemoryStream,
+                    Key = keyName,
+                    BucketName = _bucketName,
+                    ContentType = file.ContentType
+                };
+
+                var fileTransferUtility = new TransferUtility(_s3Client);
+                await fileTransferUtility.UploadAsync(uploadRequest);
+            }
+        }
+
+        public string GenerateFileLink(string keyName)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = _bucketName,
+                Key = keyName,
+                Expires = DateTime.UtcNow.AddYears(10)
+            };
+
+            request.ResponseHeaderOverrides.ContentType = "application/force-download";
+            string url = _s3Client.GetPreSignedURL(request);
+            return url;
+        }
+    }
+
+}
